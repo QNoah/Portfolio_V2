384e7f4fd3905f12b9d1be0936a874ad48a3b211
Quinten <90308786+QNoah@users.noreply.github.com>
2022-10-08 17:17:40 +0200

    Update: JS & Seperators
    Javascript iets uitgebreider mee bezig, seperators verwijderd.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 var hamburger = document.getElementById("hamburger");
 var navUL = document.getElementById("nav-links");
+var projects_image = document.getElementById("projects_image");
+
+var slide_image = 1;
 
 function hamburger_menu(){
     if(navUL.style.display == "none"){
@@ -8,5 +11,12 @@ function hamburger_menu(){
     }else{
         navUL.style.display = "none";
         hamburger.src = "../assets/imgs/mobile-portrait/hamburger.png";
+    }
+}
+function changeImage(){
+    if (projects_image.src = "./assets/imgs/projects/1.png"){
+        projects_image.src = "./assets/imgs/projects/2.png";
+    } else {
+        projects_image.src = "./assets/imgs/projects/1.png";
     }
 }
